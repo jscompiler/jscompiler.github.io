eef58d0d6bac74c97300f4d8c6f9c46d5c609b88
Subhendu Mondal <Subhendu@programmer.net>
2021-08-30 14:52:16 +0530

    DEV - vscode implemented

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,49 +1,47 @@
 (function() {
-    var myTextarea = document.getElementById("code");
-    var codeEditorDiv = document.getElementsByClassName("codeEditor");
+    require.config({ paths: { vs: '/assets/vendor/monaco-editor/min/vs' } });
+
+    var codeEditorDiv = document.getElementById("codeEditor");
     var result = document.getElementById("result");
     var errorDiv = document.getElementById("error");
 
-    var editor = CodeMirror.fromTextArea(myTextarea, {
-        lineNumbers: true,
-        value: myTextarea.innerHtml,
-        mode:  "javascript",
-        htmlMode: true,
-    });
-    
     const defaultCode = localStorage.getItem("code") || `/**
 * Write your code and
 * hit enter to execute it
 */
 console.log("Hello Dev");`;
 
-    editor.setValue(defaultCode);
-    editor.setSize(codeEditorDiv[0].clientWidth, codeEditorDiv[0].clientHeight - 2);
+    require(['vs/editor/editor.main'], function () {
+        var editor = monaco.editor.create(codeEditorDiv, {
+            value: defaultCode,
+            language: 'javascript',
+            automaticLayout: true
+        });
 
-    editor.on('change', (editor) => {
-        localStorage.setItem('code', editor.doc.getValue());
-        const code = editor.doc.getValue().replaceAll("console.log", "console.myTestLog");
-
-        try {
-
-            var allLog = [];
-            console.myTestLog = function() {
-                allLog.push(Array.from(arguments));
+        editor.onDidChangeModelContent(e => {
+            const tCode = editor.getValue();
+            localStorage.setItem('code', tCode);
+            const code = tCode.replaceAll("console.log", "console.myTestLog");
+            
+            try {
+                var allLog = [];
+                console.myTestLog = function() {
+                    allLog.push(Array.from(arguments));
+                }
+    
+                Object.prototype.toString = function () {
+                    return JSON.stringify(this);
+                }
+    
+                new Function(code)();
+                result.innerHTML = allLog.join("<br/>");
+                errorDiv.innerHTML = "";
             }
-
-            Object.prototype.toString = function () {
-                return JSON.stringify(this);
+            catch(err) {
+                const errorLine = JSON.stringify(err.stack).split("\\n").shift();
+                errorDiv.innerHTML = errorLine.replace("\"", "");
+                result.innerHTML = "";
             }
-
-            new Function(code)();
-            result.innerHTML = allLog.join("<br/>");
-            errorDiv.innerHTML = "";
-        }
-        catch(err) {
-            const errorLine = JSON.stringify(err.stack).split("\\n").shift();
-            errorDiv.innerHTML = errorLine.replace("\"", "");
-            result.innerHTML = "";
-        }
-        
+        });
     });
 })();
